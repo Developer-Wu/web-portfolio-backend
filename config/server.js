6fd944f64c2efc1abe639c0c5ece4b8fba92e696
Dan Wu <developerdanwu@gmail.com>
2021-09-05 23:30:20 +0800

    fix url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-
+  url: 'https://developerdanwu.com/backend',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '88f3385a11a78420e2eab47cee7a0f2a'),
